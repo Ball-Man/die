d3d066f24ab98b8d8b5b4cac1343db5688c87918
Francesco Mistri <francesco.mistri98@gmail.com>
2020-05-23 01:34:25 +0200

    DrawerLayout partially working

--- app/src/main/java/it/fmistri/dontdieplease/MainActivity.java
+++ app/src/main/java/it/fmistri/dontdieplease/MainActivity.java
@@ -1,16 +1,16 @@
 package it.fmistri.dontdieplease;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
-import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 import it.fmistri.dontdieplease.db.Category;
 import it.fmistri.dontdieplease.db.DieDatabase;
@@ -18,24 +18,47 @@ import it.fmistri.dontdieplease.db.DieDatabase;
 public class MainActivity extends AppCompatActivity {
     DieDatabase db;
 
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        db = DieDatabase.getInstance(this);
 
-        Button button = findViewById(R.id.button);
-        db.categoryDAO().getCategories().observe(this, new Observer<Category[]>() {
-            @Override
-            public void onChanged(Category[] categories) {
-                Log.d("DieDB", ((Integer)categories.length).toString());
-            }
-        });
+//        // Populate drawer
+//        drawerLayout = findViewById(R.id.activity_main);
+//
+        // Attach and configure the toolbar to the actionbar
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        Log.d("TOOLBAR", ((Toolbar) findViewById(R.id.toolbar)).toString());
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//        // Init database
+//        db = DieDatabase.getInstance(this);
+//
+//        Button button = findViewById(R.id.button);
+//        db.categoryDAO().getCategories().observe(this, new Observer<Category[]>() {
+//            @Override
+//            public void onChanged(Category[] categories) {
+//                Log.d("DieDB", ((Integer)categories.length).toString());
+//            }
+//        });
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
 
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
