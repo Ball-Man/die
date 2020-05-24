10327d18385703d3d97044897e6a3a33e7024fb8
BallMan <francesco.mistri98@gmail.com>
2020-05-24 18:04:15 +0200

    Better insert report dialog
    Added a proper layout with some reuse.
    The report dialog can also be used to edit reports. This is an option that will be added as soon as a calendar view is on and running.
    The 'done' text view dismisses the dialog.

--- app/src/main/java/it/fmistri/dontdieplease/dialog/ReportDialogFragment.java
+++ app/src/main/java/it/fmistri/dontdieplease/dialog/ReportDialogFragment.java
@@ -1,9 +1,15 @@
 package it.fmistri.dontdieplease.dialog;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +21,48 @@ public class ReportDialogFragment extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Set dialog title
+        getDialog().setTitle(R.string.dialog_report_title);
+
         return inflater.inflate(R.layout.dialog_report, container);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Set callback for the 'done' button
+        ((TextView) view.findViewById(R.id.done)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                done();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Set dialog size
+        Window window = getDialog().getWindow();
+        Point size = new Point();
+
+        Display display = window.getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+
+        int width = (int) (size.x * 0.85);
+
+        window.setLayout(width, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+    }
+
+    /**
+     * Submit the collected data(report) and dismiss.
+     * Callback for the done button click.
+     */
+    private void done() {
+        // TODO: submit collected data(report)
+        dismiss();
     }
 }
